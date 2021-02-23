4cb0799451da2988ed0890a0844fbcfeaec38831
rachwalk <kajetan.rachwal@mailfence.com>
2021-02-23 14:49:01 +0100

    Added session counter for accesing store/index

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,10 @@
 class StoreController < ApplicationController
   def index
     @products = Product.order(:title)
+    if session[:counter].nil?
+      session[:counter] = 0
+    end
+    @counter = session[:counter]
+    session[:counter] = session[:counter]+1
   end
 end
