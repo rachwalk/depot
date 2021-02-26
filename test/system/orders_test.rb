b58a2aafcca337c5ca1f22306573f35ef0a26d3f
rachwalk <kajetan.rachwal@mailfence.com>
2021-02-26 15:04:08 +0100

    Added authentication and authorization

--- test/system/orders_test.rb
+++ test/system/orders_test.rb
@@ -66,7 +66,6 @@ class OrdersTest < ApplicationSystemTestCase
 
     select 'Check', from: 'Pay type'
 
-
     assert_selector '#order_routing_number'
     fill_in "Routing #", with: "123456"
     fill_in "Account #", with: "987654"
