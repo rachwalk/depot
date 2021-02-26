603026d68f1944441739292907e86d705833b565
rachwalk <kajetan.rachwal@mailfence.com>
2021-02-26 15:24:04 +0100

    Added basic locale

--- config/routes.rb
+++ config/routes.rb
@@ -16,9 +16,15 @@ Rails.application.routes.draw do
     end 
   end
   resources :carts
-  root 'store#index', as: 'store_index'
+  root 'store#index'
   resources :products do
     get :who_bought, on: :member
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  scope '(:locale)' do
+    resources :orders
+    resources :line_items
+    resources :carts
+    root 'store#index', as: 'store_index', via: :all
+  end
 end
